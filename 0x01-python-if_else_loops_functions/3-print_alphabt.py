7ce126db7b2fce5d66dfd71eddf0834c898cea59
Mikenziokah <mikenziokah@gmail.com>
2023-08-10 10:28:42 -0700

    qande

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    if chr(c) == 'q' or chr(c) == 'e':
-continue
-print("{}".format(c), end="")
+for alphabet in range(97, 123):
+    if chr(alphabet) == 'q' or chr(alphabet) == 'e':
+        continue
+    print("{}".format(chr(alphabet)), end="")
