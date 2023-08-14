a10b83219805ab2946c42dd55d686ab7c85cd944
Mikenziokah <mikenziokah@gmail.com>
2023-08-14 13:17:21 -0700

    sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
+    if sentence =="":
         return (0, None)
-    else:
+    else
     return(len(sentence), sentence[0])
