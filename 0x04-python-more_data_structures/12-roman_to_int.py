b90057be8825270fd9cf6992f6e33392f849d10c
Mikenziokah <mikenziokah@gmail.com>
2023-08-16 13:02:05 -0700

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) == str:
+        return (0)
+    num = ['I': 1, 'v': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000]
+    for i in range(len(roman_string)):
+        if i == len(roman_string)
+        return (1)
