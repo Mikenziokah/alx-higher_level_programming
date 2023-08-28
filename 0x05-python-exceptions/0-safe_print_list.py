66db8c55fbf76fcf37a718ae433b0dab2c5da5e4
Mikenziokah <mikenziokah@gmail.com>
2023-08-28 18:00:14 +0000

    ident fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-        nb_print = 0
+        number = 0
         for i in range(x):
                 try:
                         print("{}".format(my_list[i]), end="")
-                except:
+                except IndexError:
                         break
                 else:
-                        nb_print += 1
+                        number += 1
 
         print()
-        return (nb_print)
+        return (number)
