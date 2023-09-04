e25bf4c2d8c71208fb3780be1b0fc3a7d7451685
Mikenziokah <mikenziokah@gmail.com>
2023-09-04 11:02:06 -0700

    rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/pytho3
+"""
+New class Rectangle
+
+"""
+
+
 class Rectangle:
-    """
-
-    defines a rectangle.
-
-    """
+    """Rectangle class empty"""
     pass
