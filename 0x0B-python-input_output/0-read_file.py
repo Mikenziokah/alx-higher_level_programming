9293dd49b5a8b6497599ab328abd7fcadb391b2e
Mikenziokah <mikenziokah@gmail.com>
2023-09-12 11:19:12 -0700

    filew

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ function that reads a text file and prints it to sdou"""
 def read_file(filename=""):
-    """ reads UTF8 file """
-    with open(filename, encoding='utf-8') as f:
-        print(f.read(), end="")
+    """ reads UTF-8 file """
+    with open(filename, encoding='utf-8') as my_file:
+        my_file_read = my_file.read()
+        print(my_file_read, end="")
