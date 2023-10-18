f290ca807e5b27a706bc3b06c9e75fb538484634
Mikenziokah <mikenziokah@gmail.com>
2023-10-18 12:42:34 -0700

    tvshows2

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,3 @@
 -- list all shows contained in the database hbtn_0d_tvshows
 -- Lists shows that have at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows_title, tv_show_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows_title, tv_show_genres.genre_id;
