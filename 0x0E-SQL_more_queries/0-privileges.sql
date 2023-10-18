3527dab39ba637254a21ee989b3cc7db1f8fa073
Mikenziokah <mikenziokah@gmail.com>
2023-10-18 10:37:11 -0700

    privilages

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- listing user privilages
--- for user_0d_1 privilages
+-- for 'user_0d_1' privilages
 SHOW GRANTS FOR user_0d_1@localhost;
---for user_0d_2 privilages
-SHOW GRANTS FOR user_d0_2@localhost;
+--for 'user_0d_2' privilages
+SHOW GRANTS FOR user_d0_2@localhosti;
