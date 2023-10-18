337c32fce461c50050027887bc3ce3f6d0a52b0c
Mikenziokah <mikenziokah@gmail.com>
2023-10-18 11:37:40 -0700

    table in database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- creating a databse and a table inside it with values 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
---- creating a table init with values
+-- creating a table init with values
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL UNIQUE AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id));
