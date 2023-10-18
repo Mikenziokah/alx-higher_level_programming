9dc70bf2cc5004054db48639ddba1ef603377dcd
Mikenziokah <mikenziokah@gmail.com>
2023-10-18 10:49:54 -0700

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- script creats a new user with a password with all privilages
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost;
