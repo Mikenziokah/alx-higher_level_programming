dc58faf3c0bb9b67f6ef190311baea3012eff2d5
Mikenziokah <mikenziokah@gmail.com>
2023-11-14 10:59:57 -0800

    3rec

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor (w, h) {
-		if (w > 0 && h > 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-	print () {
-		let x = 0;
-		for (x; x < this.height; x++) {
-			console.log('X'.repeat(this.width));
-		}
-	}
+  print () {
+    let x = 0;
+    for (x; x < this.height; x++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 }
 module.exports = Rectangle;
