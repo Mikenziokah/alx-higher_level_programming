a305a7320b40c19a9ba0c06f4283035637613cac
Mikenziokah <mikenziokah@gmail.com>
2023-12-01 10:17:25 -0800

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 if __name__ == '__main__':
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        r = response.read()
+        r = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(r)))
         print("\t- content: {}".format(r))
