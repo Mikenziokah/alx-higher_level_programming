a741b6df2081c82085b7092d00449ce637a7d699
Mikenziokah <mikenziokah@gmail.com>
2024-01-07 01:01:27 -0800

    reademe1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,10 +2,10 @@
 const fs = require('fs');
 const argms = process.argv[2];
 
-fs.readFile(argms, 'utf-8', (err, data) => {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log(data.toString());
-	}
+fs.readFile(argms, 'utf-8', function (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
